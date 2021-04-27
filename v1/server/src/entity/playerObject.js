017fb0cb05e1ba995b4d5b26696a18b69e76f09b
Seunghyun Yeo <shyeo0517@gmail.com>
2021-04-27 13:58:44 +0900

    player

--- v1/server/src/entity/playerObject.js
+++ v1/server/src/entity/playerObject.js
@@ -1,6 +1,9 @@
 import { InputDeque } from "../util/deque";
 import { WallBlock } from "./blockObject";
 
+// define const Flash area of player
+const FlashArea = Object.freeze({'w':[[0,1]], 's':[[0,-1]], 'd':[[1,0]], 'a':[[-1,0]]});
+
 export class PlayerObject {
     constructor(socketID, AA, name, x, y) {
         this.socketID = socketID;
@@ -14,6 +17,8 @@ export class PlayerObject {
 
         this.dir = { x: 0, y: -1 };
         this.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        this.canmove = true;
+        this.usingFlash = false;
     }
 
     getItems(items) {
@@ -28,6 +33,7 @@ export class PlayerObject {
     isSolved(problemID) {
         if (problemID in this.solvedProblemIDs) {
             return true
+        }
     }
 
     canPass(block) {
@@ -40,7 +46,12 @@ export class PlayerObject {
         } else if (block instanceof ProblemBlock) {
             return false // problem is kind of box so player can't go through
         } else {
-            console.log('Block type error')
+            console.log('Block type error');
         }
+    }
+
+    inFlashArea(block) {
+        var currentFlashArea = FlashArea[this.dir];
+        return [block.x - player.x, block.y - player.y] in currentFlashArea
     }
 }
