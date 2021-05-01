26b9e70d9eaab8d08480c23921c3c09571b6d6d0
PositivePeriod <positiveperiod.dev@gmail.com>
2021-05-02 08:51:32 +0900

    feat: more people

--- v1/server/src/constant.js
+++ v1/server/src/constant.js
@@ -17,7 +17,19 @@ const MSG = Object.freeze({
     DISCONNECT_SERVER: "disconnect",
 });
 
-const AAtoCODE = Object.freeze(new Map([["AA1_1", "CDQE"],["AA1_2", "DKRO"],["AA2_1", "CVPF"]]));
+const AAtoCODE = Object.freeze(new Map([
+    ["AA1_1", "CDQE"],
+    ["AA1_2", "DKRO"],
+    ["AA1_3", "CVPF"],
+    ["AA1_4", "CDQE"],
+    ["AA1_5", "IOPP"],
+    ["AA2_6", "JJOK"],
+    ["AA1_7", "HOKO"],
+    ["AA1_9", "OHGI"],
+    ["AA2_10", "RUYI"],
+    ["AA1_11", "CDQE"],
+    ["AA1_12", "DKRO"],
+]));
 
 var reverse = {};
 for (let [key, value] of Object.entries(AAtoCODE)) {
@@ -27,7 +39,7 @@ const CODEtoAA = Object.freeze(reverse);
 
 const PROBLEMS = {
     "noblanks": { "hint": "noblanks", "answer": 428 },
-    "convenience_store": {"hint": "편의점", "answer": "SEVEN ELEVEN" },
+    "convenience_store": { "hint": "편의점", "answer": "SEVEN ELEVEN" },
     "identity_element": { "hint": "항등원", "answer": ["1^111", "1*111"], "type": "or" },
     "addition": { "hint": "파이썬 함수", "answer": "SUMMARY" },
     "ans_in_problem": { "hint": "C=see", "answer": "LONG TIME NO SEE" },
@@ -47,15 +59,15 @@ const PROBLEMS = {
     "guessing_word": { "hint": "1번째 글자는 M이다", "answer": "MESSAGE" },
     "mugcup": { "hint": "손잡이의 방향에 주목하라", "answer": "FORTUNE FAVORS THE BRAVE" },
     "keypad": { "hint": "폴더폰", "answer": "들어가면 죽어" },
-    "cross_word_puzzle": { "hint": null, "answer": {1:'',2:'',3:'',4:'',5:'',6:'',7:'',8:'',9:'',10:'' } },
+    "cross_word_puzzle": { "hint": null, "answer": { 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '', 10: '' } },
     "formula_conversion": { "hint": "제곱", "answer": 10 },
     "pyramid": { "hint": "아래에서 네 번째 행의 빈칸을 x,y,z로 설정한다", "answer": 282 },
     "annual": { "hint": null, "answer": ["3월 26일 17시 48분 59초", "9월 28일 17시 56분 43초"], "type": "and" },
     "four_character_idioms": { "hint": null, "answer": "확인필요" },
-    "variable": { "hint": "그냥 푸세요 =D", "answer": {"A":9876, "B":5432} },
+    "variable": { "hint": "그냥 푸세요 =D", "answer": { "A": 9876, "B": 5432 } },
     "arrangement": { "hint": "나홀로 집에", "answer": "H" },
     "password": { "hint": "밀자", "answer": "CURSE" },
     "number": { "hint": "영어로", "answer": 3 }
 };
 
-module.exports = { MSG, AAtoCODE, CODEtoAA, PROBLEMS};
+module.exports = { MSG, AAtoCODE, CODEtoAA, PROBLEMS };
